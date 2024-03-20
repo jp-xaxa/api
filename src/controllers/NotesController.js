46de0f740d7f6eb4a67008d9a22a3f56fb5c773e
Joao Pedro <joaopedrolamim@yahoo.com.br>
2024-03-20 19:02:57 -0300

    updated functions

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -68,6 +68,7 @@ class NotesController {
         .whereLike("notes.title", `%${title}%`)
         .whereIn("name", filterTags)
         .innerJoin("notes", "notes.id", "tags.note_id")
+        .groupBy("notes.title")
         .orderBy("notes.title")
     } else {
       notes = await knex("notes")
